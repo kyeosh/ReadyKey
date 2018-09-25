67a22297cd96c8fd0b86649067a8efcab725438c
kyeosh <tdcoxe@gmail.com>
2018-09-25 10:55:12 -0700

    Update tmu.sh

--- tmu.sh
+++ tmu.sh
@@ -1,3 +1,3 @@
 /usr/bin/tmux new-session -d -s detect
 /usr/bin/tmux set-option -t detect set-remain-on-exit on
-/usr/bin/tmux new-windows -d -n 'GPSdetect' -t detect:1 'sudo python /home/pi/kyeocycle/gpskeydetect.py'
+/usr/bin/tmux new-window -d -n 'GPSdetect' -t detect:1 'sudo python /home/pi/kyeocycle/gpskeydetect.py'
