c4a3de92c1001a481bed394f306fa090b928b2c9
kyeosh <tdcoxe@gmail.com>
2019-01-09 11:39:09 -0800

    Update depends.sh

--- depends.sh
+++ depends.sh
@@ -1,1 +1,2 @@
-sudo apt-get install wiringpi bluez python-bluez ntpdate tmux gpsd gpsd-clients python-gps rclone
+sudo apt-get install wiringpi bluez python-bluez ntpdate tmux gpsd gpsd-clients python-gps rclone python-pip
+sudo pip install rpi_backlight
