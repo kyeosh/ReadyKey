939f90eddd3d3943065fd01ae432052c5d9d5bfa
kyeosh <tdcoxe@gmail.com>
2019-03-07 12:32:01 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,11 +19,19 @@ echo $input>/home/pi/readykey/KeyID
 #step 4: run the rclone configuration script
 #currently must run be configured over vnc or with a display to log in to google drive via a browser
 # install raspberrypi-ui-mods and chromium-browser to raspian lite before running this script
-rclone config create cloud drive
-
+echo "Do you wish to install this program?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) clone config create cloud drive 
 #step 5: configure crontab to run rclone every 15 mins. check the second answer
 #  https://stackoverflow.com/questions/8579330/appending-to-crontab-with-a-shell-script-on-ubuntu  
-crontab -r
-(crontab -l ; echo "0,15,30,45 * * * * /home/pi/readykey/logsync.sh")| crontab -
-(crontab -l ; echo "0 1 * * * sudo find /home/pi/readykey/log/ -mtime +7 -type f -delete")| crontab -
-(crontab -l ; echo "0 3 * * * sudo reboot")| crontab -
+              crontab -r
+              (crontab -l ; echo "0,15,30,45 * * * * /home/pi/readykey/logsync.sh")| crontab -
+              (crontab -l ; echo "0 1 * * * sudo find /home/pi/readykey/log/ -mtime +7 -type f -delete")| crontab -
+              (crontab -l ; echo "0 3 * * * sudo reboot")| crontab -
+        No ) exit;;
+    esac
+done
+
+
+
